6416e30e420a2aeea5e059ec82590c8b84c5d141
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-08-05 00:38:06 +0100

    Add babel webpack build configurations

--- index.js
+++
@@ -1,2 +0,0 @@
-export { default as SnackbarProvider } from './src/SnackbarProvider';
-export { default as withSnackbar } from './src/withSnackbar';
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as SnackbarProvider } from './SnackbarProvider';
+export { default as withSnackbar } from './withSnackbar';
