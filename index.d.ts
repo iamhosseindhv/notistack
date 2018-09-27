07f77061b2bc58e7359907835367c66d3ea520ac
Kevin Gonnord <kevin.gonnord@outlook.com>
2018-09-27 17:54:52 +0200

    Add MuiSnackbar Props

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { TransitionActions } from "react-transition-group/Transition";
 import { SvgIconProps } from "@material-ui/core/SvgIcon";
+import { SnackbarProps } from "@material-ui/core/Snackbar"
 
 export type MessageType = "error" | "success" | "warning" | "info"
 
@@ -13,10 +14,9 @@ export interface InjectedNotistackProps {
 export function withSnackbar<P extends InjectedNotistackProps>(component: React.ComponentType<P>):
   React.ComponentClass<Pick<P, Exclude<keyof P, keyof InjectedNotistackProps>>> & { WrappedComponent: React.ComponentType<P> };
 
-export interface SnackbarProviderProps {
+export interface SnackbarProviderProps extends SnackbarProps {
   maxSnack: number;
   iconVariant?: React.ComponentType<SvgIconProps>;
-  transitionDuration?: number | { enter?: number, exit?: number };
 }
 
 export const SnackbarProvider: React.ComponentType<SnackbarProviderProps>;
