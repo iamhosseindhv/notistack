4a38d8de49149417633ed556543f7c7e3cbad2b1
Kevin Gonnord <kevin.gonnord@outlook.com>
2018-09-27 18:08:13 +0200

    Remove "open" from props

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,8 @@ import { TransitionActions } from "react-transition-group/Transition";
 import { SvgIconProps } from "@material-ui/core/SvgIcon";
 import { SnackbarProps } from "@material-ui/core/Snackbar"
 
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
 export type MessageType = "error" | "success" | "warning" | "info"
 
 export interface InjectedNotistackProps {
@@ -12,11 +14,13 @@ export interface InjectedNotistackProps {
 }
 
 export function withSnackbar<P extends InjectedNotistackProps>(component: React.ComponentType<P>):
-  React.ComponentClass<Pick<P, Exclude<keyof P, keyof InjectedNotistackProps>>> & { WrappedComponent: React.ComponentType<P> };
+  React.ComponentClass<Omit<P, keyof InjectedNotistackProps>> & { WrappedComponent: React.ComponentType<P> };
 
-export interface SnackbarProviderProps extends SnackbarProps {
+  
+export interface SnackbarProviderProps extends Omit<SnackbarProps, "open">  {
   maxSnack: number;
   iconVariant?: React.ComponentType<SvgIconProps>;
+  transitionDuration?: number | { enter?: number, exit?: number };
 }
 
 export const SnackbarProvider: React.ComponentType<SnackbarProviderProps>;
