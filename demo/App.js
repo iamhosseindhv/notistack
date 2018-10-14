32d9df2e8bac93ad56d464b0e7aa504e052d0573
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-10-14 02:46:59 +0100

    Simplify codesandbox demo to be minimal working example

--- demo/App.js
+++ demo/App.js
@@ -1,71 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react';
 import { SnackbarProvider } from 'notistack';
 import MessageButtons from './MessageButtons';
-import ConfigButtons from './ConfigButtons';
 
-const styles = {
-    root: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#fafafa',
-    },
-};
 
-class App extends Component {
-    state = {
-        anchorVertical: 'bottom',
-        anchorHorizontal: 'left',
-        autoHideDuration: 5000,
-        maxSnack: 3,
-    };
-
-    handleChangeInput = (key, value) => {
-        this.setState({ [key]: value });
-    };
-
-    handleChangeRadio = event => (
-        this.setState({ [event.target.name]: event.target.value })
-    );
-
-    render() {
-        const { classes } = this.props;
-        const {
-            maxSnack,
-            anchorVertical,
-            anchorHorizontal,
-            autoHideDuration,
-        } = this.state;
-
-        return (
-            <SnackbarProvider
-                maxSnack={maxSnack}
-                autoHideDuration={autoHideDuration}
-                anchorOrigin={{
-                    vertical: anchorVertical,
-                    horizontal: anchorHorizontal,
-                }}
-            // transitionDuration={{ exit: 380, enter: 400 }}
-            >
-                <div className={classes.root}>
-                    <ConfigButtons
-                        maxSnack={maxSnack}
-                        anchorVertical={anchorVertical}
-                        anchorHorizontal={anchorHorizontal}
-                        autoHideDuration={autoHideDuration}
-                        onChangeRadio={this.handleChangeRadio}
-                        onChangeInput={this.handleChangeInput}
-                    />
-                    <MessageButtons />
-                </div>
-            </SnackbarProvider>
-        );
-    }
-}
-
-App.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(App);
+export default () => (
+    <SnackbarProvider maxSnack={3}>
+        <MessageButtons />
+    </SnackbarProvider>
+);
--- demo/MessageButtons.js
+++ demo/MessageButtons.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import { withSnackbar } from 'notistack';
 
-const styles = theme => ({
+const styles = {
     root: {
         flexGrow: 1,
         display: 'flex',
-        margin: 8 * 2,
+        margin: 16,
         justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: '#f2f2f2',
+        alignItems: 'middle',
     },
     button: {
-        margin: theme.spacing.unit,
+        margin: 8,
         color: '#fff',
+        backgroundColor: '#313131',
     },
     success: {
         backgroundColor: '#43a047',
@@ -33,42 +31,60 @@ const styles = theme => ({
     warning: {
         backgroundColor: '#ffa000',
     },
-});
+};
 
 const buttons = [
-    { variant: 'default', message: 'Customise this snackbar youself.' },
     { variant: 'success', message: 'Successfully done the operation.' },
     { variant: 'error', message: 'Something went wrong.' },
     { variant: 'warning', message: 'Be careful of what you just did!' },
     { variant: 'info', message: 'For your info...' },
 ];
 
-const MessageButtons = ({ classes, enqueueSnackbar }) => (
-    <div className={classes.root}>
-        <Grid container spacing={24}>
-            <Grid item xs md />
-            <Grid item xs={12} md={6}>
-                <Paper className={classes.paper} elevation={2}>
-                    {buttons.map(button => (
-                        <Button
-                            key={button.variant}
-                            variant="contained"
-                            className={classNames(classes.button, classes[button.variant])}
-                            onClick={() => enqueueSnackbar(button.message, { variant: button.variant })}
-                        >
-                            {button.variant}
-                        </Button>
-                    ))}
-                </Paper>
-            </Grid>
-            <Grid item xs md />
-        </Grid>
-    </div>
-);
+
+class MessageButtons extends Component {
+    handleClick = button => () => {
+        this.props.enqueueSnackbar(button.message, { variant: button.variant });
+    };
+
+    handleClickWithAction = () => {
+        this.props.enqueueSnackbar('Customise this snackbar youself.', {
+            variant: 'default',
+            action: <Button color="secondary" size="small">Dismiss</Button>,
+            // onClickAction: () => alert('uncomment me to have your custom event handler'),
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Paper className={classes.root}>
+                {buttons.map(button => (
+                    <Button
+                        key={button.variant}
+                        variant="contained"
+                        className={classNames(classes.button, classes[button.variant])}
+                        onClick={this.handleClick(button)}
+                    >
+                        {button.variant}
+                    </Button>
+                ))}
+                <Button
+                    variant="contained"
+                    className={classes.button}
+                    onClick={this.handleClickWithAction}
+                >
+                    default
+                </Button>
+            </Paper>
+        );
+    }
+}
 
 MessageButtons.propTypes = {
     classes: PropTypes.object.isRequired,
     enqueueSnackbar: PropTypes.func.isRequired,
 };
 
-export default withSnackbar(withStyles(styles)(MessageButtons));
+export default withStyles(styles)(
+    withSnackbar(MessageButtons),
+);
