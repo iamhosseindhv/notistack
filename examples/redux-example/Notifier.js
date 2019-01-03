6a3cb2e0f6293db4a19d2d24bc622bbcb86a56c6
Sudhanshu <sudhansh@gmail.com>
2019-01-03 19:36:59 +0530

    Improved redux example
    - Removed the ugly hack of setTimeout
    - Removed unncessary renders
    - `displayed` does not need to be a state

--- examples/redux-example/Notifier.js
+++ examples/redux-example/Notifier.js
@@ -5,33 +5,39 @@ import { withSnackbar } from 'notistack';
 import { removeSnackbar } from './redux/actions';
 
 class Notifier extends Component {
-    state = {
-        displayed: [],
+    displayed = [];
+    storeDisplayed = id => {
+        this.displayed = [...this.displayed, id];
     };
+    shouldComponentUpdate({ notifications: nn = [] }) {
+        const { notifications: cn } = this.props;
+        let notExists = false;
+        for (let i = 0; i < nn.length; i++) {
+            if (notExists) continue;
+            notExists =
+                notExists || !cn.filter(({ key }) => nn[i].key === key).length;
+        }
+        return notExists;
+    }
+    componentDidUpdate() {
+        const {
+            notifications = [],
+            enqueueSnackbar,
+            removeSnackbar: rs
+        } = this.props;
 
-    storeDisplayed = (key) => {
-        this.setState(({ displayed }) => ({
-            displayed: [...displayed, key],
-        }));
-    };
-
-    render() {
-        const { notifications, enqueueSnackbar, removeSnackbar } = this.props;
-        const { displayed } = this.state;
-
-        notifications.forEach((notification) => {
-            setTimeout(() => {
-                // If notification already displayed, abort
-                if (displayed.indexOf(notification.key) > -1) return;
-                // Display notification using notistack
-                enqueueSnackbar(notification.message, notification.options);
-                // Add notification's key to the local state
-                this.storeDisplayed(notification.key);
-                // Dispatch action to remove the notification from the redux store
-                removeSnackbar(notification.key);
-            }, 1);
+        notifications.forEach(notification => {
+            // If notification already displayed, abort
+            if (this.displayed.indexOf(notification.key) > -1) return;
+            // Display notification using notistack
+            enqueueSnackbar(notification.message, notification.options);
+            // Add notification's id to the local state
+            this.storeDisplayed(notification.key);
+            // Dispatch action to remove the notification from the redux store
+            rs(notification.key);
         });
-
+    }
+    render() {
         return null;
     }
 }
