e852c141f89f7ba1dfa06d3a166f45233fd4a7b2
Sudhanshu <sudhansh@gmail.com>
2019-01-04 00:53:09 +0530

    Enhance Redux example (#41)
    * Improved redux example
    * Minor improvements to redux example

--- examples/redux-example/Notifier.js
+++ examples/redux-example/Notifier.js
@@ -5,33 +5,37 @@ import { withSnackbar } from 'notistack';
 import { removeSnackbar } from './redux/actions';
 
 class Notifier extends Component {
-    state = {
-        displayed: [],
+    displayed = [];
+
+    storeDisplayed = id => {
+        this.displayed = [...this.displayed, id];
     };
 
-    storeDisplayed = (key) => {
-        this.setState(({ displayed }) => ({
-            displayed: [...displayed, key],
-        }));
-    };
+    shouldComponentUpdate({ notifications: newSnacks = [] }) {
+        const { notifications: currentSnacks } = this.props;
+        let notExists = false;
+        for (let i = 0; i < newSnacks.length; i++) {
+            if (notExists) continue;
+            notExists = notExists || !currentSnacks.filter(({ key }) => newSnacks[i].key === key).length;
+        }
+        return notExists;
+    }
 
-    render() {
-        const { notifications, enqueueSnackbar, removeSnackbar } = this.props;
-        const { displayed } = this.state;
+    componentDidUpdate() {
+        const { notifications = [] } = this.props;
 
-        notifications.forEach((notification) => {
-            setTimeout(() => {
-                // If notification already displayed, abort
-                if (displayed.indexOf(notification.key) > -1) return;
-                // Display notification using notistack
-                enqueueSnackbar(notification.message, notification.options);
-                // Add notification's key to the local state
-                this.storeDisplayed(notification.key);
-                // Dispatch action to remove the notification from the redux store
-                removeSnackbar(notification.key);
-            }, 1);
+        notifications.forEach(notification => {
+            // Do nothing if snackbar is already displayed
+            if (this.displayed.includes(notification.key)) return;
+            // Display snackbar using notistack
+            this.props.enqueueSnackbar(notification.message, notification.options);
+            // Keep track of snackbars that we've displayed
+            this.storeDisplayed(notification.key);
+            // Dispatch action to remove snackbar from redux store
+            this.props.removeSnackbar(notification.key);
         });
-
+    }
+    render() {
         return null;
     }
 }
