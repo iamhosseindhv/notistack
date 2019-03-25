fcc6ad0cc1c06cb42e34103067f01054b37283ea
Lukas Kullmann <lukas.kullmann@kiwigrid.com>
2019-03-25 14:53:38 +0100

    Add displayName to withSnackbar HOC fixes #99

--- src/withSnackbar.js
+++ src/withSnackbar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { SnackbarContext, SnackbarContextNext } from './SnackbarContext';
 
-const withSnackbar = Component => props => (
-    <SnackbarContext.Consumer>
-        {handlePresentSnackbar => (
-            <SnackbarContextNext.Consumer>
-                {context => (
-                    <Component
-                        {...props}
-                        onPresentSnackbar={handlePresentSnackbar}
-                        enqueueSnackbar={context.handleEnqueueSnackbar}
-                        closeSnackbar={context.handleCloseSnackbar}
-                    />
-                )}
-            </SnackbarContextNext.Consumer>
-        )}
-    </SnackbarContext.Consumer>
-);
+const withSnackbar = (WrappedComponent) => {
+    const Component = props => (
+        <SnackbarContext.Consumer>
+            {handlePresentSnackbar => (
+                <SnackbarContextNext.Consumer>
+                    {context => (
+                        <WrappedComponent
+                            {...props}
+                            onPresentSnackbar={handlePresentSnackbar}
+                            enqueueSnackbar={context.handleEnqueueSnackbar}
+                            closeSnackbar={context.handleCloseSnackbar}
+                        />
+                    )}
+                </SnackbarContextNext.Consumer>
+            )}
+        </SnackbarContext.Consumer>
+    );
+
+    Component.displayName = `withSnackbar(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+    return Component;
+};
 
 export default withSnackbar;
