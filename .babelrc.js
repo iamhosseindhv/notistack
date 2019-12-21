df514c9c2e5322dc6d0b6ac28b4b4e3a5cf8a313
merceyz <merceyz@users.noreply.github.com>
2019-12-21 20:50:41 +0100

    chore: add transform-runtime

--- .babelrc.js
+++ .babelrc.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties",
+    [
+      "@babel/plugin-transform-runtime",
+      { version: require("@babel/runtime/package.json").version }
+    ]
   ],
   env: {
     production: {
