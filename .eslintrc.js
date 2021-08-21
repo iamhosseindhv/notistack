fe20b1a80382de62347f89803abd0eef5cf05c9e
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2021-08-21 18:25:47 +0100

    Bind this

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
         'react/destructuring-assignment': 0,
         'react/jsx-closing-bracket-location': 0,
         'react/sort-comp': 0,
+        'arrow-parens': 0,
         'react/require-default-props': 0,
         'react/jsx-props-no-spreading': 0,
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
