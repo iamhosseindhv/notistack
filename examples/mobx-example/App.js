6d011aa91cb9efdd3dceb3a46826903d84cc9f1c
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2021-09-25 15:20:59 +0100

    Update mobx example

--- examples/mobx-example/App.js
+++ examples/mobx-example/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { inject, observer } from 'mobx-react';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Notifier from './Notifier';
 
 class App extends Component {
