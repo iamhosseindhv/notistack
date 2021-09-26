d877df68c6b91343f0d8cd15425386510492713e
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2021-09-26 16:17:46 +0100

    Fix ts-ignore

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,8 +1,10 @@
 import { css, CSSAttribute } from 'goober';
 
 export function makeStyles<S extends { [key: string]: CSSAttribute }, K extends keyof S>(styles: S): { [key in K]: string } {
-    // @ts-ignore
-    return Object.entries(styles).reduce((acc, [key, value]) => ({ ...acc, [key]: css(value) }), {});
+    return Object.entries(styles).reduce((acc, [key, value]) => ({
+        ...acc,
+        [key]: css(value),
+    }), {} as { [key in K]: string });
 }
 
 export const ComponentClasses = {
