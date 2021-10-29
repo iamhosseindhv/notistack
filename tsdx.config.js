3bf8ea07f46d5c8ca6d26c2e46b5c1eb781db135
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2021-10-30 00:27:31 +0100

    Copy files to dist folder

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,23 @@
 const copy = require('rollup-plugin-copy');
 const bundleSize = require('rollup-plugin-bundle-size');
 
+const DIST_FOLDER = 'dist';
+
 module.exports = {
     rollup(config) {
         config.plugins.push(bundleSize());
-        // copy decleration file over
-        config.plugins.push(copy({ targets: [{ src: 'src/types.ts', dest: 'dist', rename: 'index.d.ts' }] }));
+        config.plugins.push(
+            copy({
+                targets: [
+                    // copy decleration file over
+                    { src: 'src/types.ts', dest: DIST_FOLDER, rename: 'index.d.ts' },
+                    // and a bunch of other things
+                    { src: 'package.json', dest: DIST_FOLDER },
+                    { src: 'README.md', dest: DIST_FOLDER },
+                    { src: 'LICENSE.md', dest: DIST_FOLDER },
+                ]
+            })
+        );
         return config;
     },
 };
