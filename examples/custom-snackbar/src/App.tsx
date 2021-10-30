9ef627513bb9d6781e7006479a8da728fbf5afce
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2021-10-30 22:02:26 +0100

    Update indentation from 4 to 2 spaces

--- examples/custom-snackbar/src/App.tsx
+++ examples/custom-snackbar/src/App.tsx
@@ -4,53 +4,53 @@ import ThemeResponsiveSnackbar from "./ThemeResponsiveSnackbar";
 import { createContext, useContext, useState } from "react";
 
 const Child = () => {
-    const { enqueueSnackbar } = useSnackbar();
-    return (
-        <button
-            style={{ margin: "0 16px" }}
-            onClick={() => enqueueSnackbar("I like snackbars")}
-        >
-            Show snackbar
-        </button>
-    );
+  const { enqueueSnackbar } = useSnackbar();
+  return (
+    <button
+      style={{ margin: "0 16px" }}
+      onClick={() => enqueueSnackbar("I like snackbars")}
+    >
+      Show snackbar
+    </button>
+  );
 };
 
 const ThemeProvider = createContext<string>("");
 export const useTheme = () => useContext(ThemeProvider);
 
 export default function App() {
-    const [themeType, toggleTheme] = useState("dark");
+  const [themeType, toggleTheme] = useState("dark");
 
-    const handleToggleTheme = () => {
-        toggleTheme((oldTheme) => (oldTheme === "light" ? "dark" : "light"));
-    };
+  const handleToggleTheme = () => {
+    toggleTheme((oldTheme) => (oldTheme === "light" ? "dark" : "light"));
+  };
 
-    return (
-        <ThemeProvider.Provider value={themeType}>
-            <SnackbarProvider
-                Components={{
-                    default: ThemeResponsiveSnackbar,
-                    success: ThemeResponsiveSnackbar,
-                    error: ThemeResponsiveSnackbar,
-                    warning: ThemeResponsiveSnackbar
-                }}
-            >
-                <div
-                    style={{
-                        height: "100vh",
-                        width: "100vw",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        backgroundColor: themeType === "light" ? "white" : "black"
-                    }}
-                >
-                    <Child />
-                    <button onClick={handleToggleTheme}>
-                        Toggle theme to {themeType}
-                    </button>
-                </div>
-            </SnackbarProvider>
-        </ThemeProvider.Provider>
-    );
+  return (
+    <ThemeProvider.Provider value={themeType}>
+      <SnackbarProvider
+        Components={{
+          default: ThemeResponsiveSnackbar,
+          success: ThemeResponsiveSnackbar,
+          error: ThemeResponsiveSnackbar,
+          warning: ThemeResponsiveSnackbar
+        }}
+      >
+        <div
+          style={{
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: themeType === "light" ? "white" : "black"
+          }}
+        >
+          <Child />
+          <button onClick={handleToggleTheme}>
+            Toggle theme to {themeType}
+          </button>
+        </div>
+      </SnackbarProvider>
+    </ThemeProvider.Provider>
+  );
 }
--- examples/custom-snackbar/src/index.tsx
+++ examples/custom-snackbar/src/index.tsx
@@ -1,5 +1,4 @@
 import { render } from "react-dom";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
