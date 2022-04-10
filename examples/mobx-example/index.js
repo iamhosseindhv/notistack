64b7661b06bf8cf58460b85033ec7039f9c59fb5
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2022-04-10 21:32:29 +0100

    Support React 18 (#482)
    * Update react peerDependencies
    
    * Update examples to use react 18

--- examples/mobx-example/index.js
+++ examples/mobx-example/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'mobx-react';
 import { SnackbarProvider } from 'notistack';
 import store from './store/store';
 import App from './App';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
         <SnackbarProvider>
             <App />
         </SnackbarProvider>
-    </Provider>,
-    document.getElementById('root'),
+    </Provider>
 );
