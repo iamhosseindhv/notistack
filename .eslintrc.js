f3f96a2fa2b529b43fa743f5c0505f6e3bf4929f
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2022-06-19 18:51:50 +0100

    Minor

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
     },
     extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
     parser: 'babel-eslint',
+    globals: {
+        process: true,
+    },
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
