e4407c3f4cef2d6e6b056546cf5f2997e87ff6f8
Josh Kelley <joshkel@gmail.com>
2025-08-02 22:40:41 -0400

    Node.js-compatible ESM support
    notistack ships both CJS (CommonJS) and ESM (ECMAScript Modules) versions by setting both the `main` and `module` fields of its package.json. The `module` field is a convention of bundlers such as Rollup (see https://stackoverflow.com/q/42708484/25507), but it isn't used by Node.js, and it's not used by TypeScript when TypeScript is configured to use Node.js-compatible resolution methods such as `"moduleResolution": "nodenext"`.
    
    This caused problems for our project when we switched from CommonJS to ESM; Jest (which, from what I can tell, uses Node-style resolution) was no longer able to properly resolve notistack members for our automated tests.

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,3 +1,4 @@
+const path = require('node:path');
 const copy = require('rollup-plugin-copy');
 const bundleSize = require('rollup-plugin-bundle-size');
 
@@ -6,11 +7,18 @@ const DIST_FOLDER = 'dist';
 module.exports = {
     rollup(config) {
         config.plugins.push(bundleSize());
+
+        let dts = 'index.d.ts';
+        // Write ESM as .mjs to match Node.js expectations.
+        if (config.output && config.output.format === 'esm') {
+            config.output.file = config.output.file.replace(/\.js$/, '.mjs');
+            dts = path.basename(config.output.file, '.mjs') + '.d.mts';
+        }
         config.plugins.push(
             copy({
                 targets: [
-                    // copy decleration file over
-                    { src: 'src/types.ts', dest: DIST_FOLDER, rename: 'index.d.ts' },
+                    // copy declaration file over
+                    { src: 'src/types.ts', dest: DIST_FOLDER, rename: dts },
                 ],
             })
         );
